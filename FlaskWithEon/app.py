bdae2b37766fe2e004b554f4bcda7f2ee660927e
Manash Kumar Mandal <manashmndl@gmail.com>
2017-02-17 16:13:24 +0600

    Fixed some problems

--- FlaskWithEon/app.py
+++ FlaskWithEon/app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+sensorvalue = 50
+
 @app.route('/')
 def home():
-    return render_template('graph.html')
+    return render_template('graph.html', sensor_value=sensorvalue)
 
 # @app.route('/<int:sensor_value>')
 # def send_value(sensor_value):
 #     return render_template('graph.html', sensor_value=sensor_value)
 
+sensorvalue = 0
 @app.route('/send_value', methods=['GET'])
 def send_value():
+    global sensorvalue
     a = request.args.get('a')
-    return render_template('graph.html', sensor_value=a)
+    sensorvalue = a 
+    print("The value is : " + a)
+    return sensorvalue
 
