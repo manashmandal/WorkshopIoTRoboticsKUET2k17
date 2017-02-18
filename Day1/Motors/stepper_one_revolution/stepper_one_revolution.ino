7a124bf8b1dcdbeac17f861c3d0b9e5db86b41a5
Manash Kumar Mandal <manashmndl@gmail.com>
2017-02-19 03:24:17 +0600

    Stepper codes

---
+++ Day1/Motors/stepper_one_revolution/stepper_one_revolution.ino
@@ -0,0 +1,22 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
+
